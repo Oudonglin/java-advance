57a152393466bebf8116030813a77f7e71adbe60
doney <995068823@qq.com>
2020-09-12 16:14:32 +0800

    init

---
+++ src/main/java/com/doney/advanced/AdvancedApplication.java
@@ -0,0 +1,23 @@
+package com.doney.advanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AdvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedApplication.class, args);
+	}
+
+	@RequestMapping("/hello")
+	@GetMapping
+	public String hello(){
+		return "hello";
+	}
+
+}
